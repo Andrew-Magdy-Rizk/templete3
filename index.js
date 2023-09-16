bd8e6e004132178e465540a35cc82258bb04ae87
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2023-09-16 12:32:12 +0300

    loading

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let el = document.querySelector(".scroller");
-let height = document.documentElement.scrollHeight;
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
 window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
-  el.style.width = `${(scrollTop / height) * 100 - 8}%`;
+  el.style.width = `${((scrollTop / height) * 100).toFixed(2)}%`;
+  console.log(el.style.width);
 });
