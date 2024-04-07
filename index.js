a9cc736f4d6af0d29d1a272d59ec444022c9f0d0
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2024-04-07 21:18:22 +0200

    set interval

--- index.js
+++ index.js
@@ -5,5 +5,51 @@ let height =
 window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
   el.style.width = `${((scrollTop / height) * 100).toFixed(2)}%`;
-  console.log(el.style.width);
 });
+
+// counter up
+let counters = document.querySelectorAll(".stats .box .number");
+let saction = document.querySelector(".stats");
+let started = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= saction.offsetTop - 300) {
+    if (!started) {
+      counters.forEach((counter) => startCount(counter));
+    }
+    started = true;
+  }
+  if (window.scrollY >= progresssec.offsetTop - 100) {
+    if (!startedprog) {
+      countSkill.forEach((counter) => startCountprog(counter));
+    }
+    startedprog = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+// Counter progress
+let countSkill = document.querySelectorAll(".our-skills .the-progress span");
+let progresssec = document.querySelector(".our-skills");
+let startedprog = false;
+
+function startCountprog(el) {
+  let number = 0;
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.style.width = number + "%";
+    number++;
+    if (number == goal) {
+      clearInterval(count);
+    }
+  }, 1000 / goal);
+}
